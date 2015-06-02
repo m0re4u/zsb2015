aa3f919da1320efe492ac8f3cd882e459021cc88
Michiel <michielmeer@live.nl>
2015-06-02 10:00:28 +0200

    updated moves/2

--- tictactoe.pl
+++ tictactoe.pl
@@ -9,12 +9,21 @@ include('alphabeta.pl').
 
 % List of moves from Pos, returns list of possible moves, fails if no moves are possible
 moves(Pos,PosList):-
+	bagof(X,move(Pos,X), PosList).
 
 % Value of a terminal node
 staticval(Pos, Val):-
+	
 
 % Opponents turn
-min_to_move(Pos):-
+%min_to_move(Pos):-
 
 % Your turn
-max_to_move(Pos):-
+%max_to_move(Pos):-
+
+
+move(Pos,X):-
+	select(0,Pos,1,X).
+	
+countermove(Pos,X):-
+	select(0,Pos,2,X).
