7382c40b5b6fb89d216e90706cf549663e75d2b8
Michiel <michielmeer@live.nl>
2015-06-02 10:30:30 +0200

    min / max to move

--- tictactoe.pl
+++ tictactoe.pl
@@ -52,12 +52,18 @@ returnstaticval(A,Val):-
 	A == 2,
 	Val = -1.	
 
+% count/2 for counting 0's
+count([],X,0).
+count([X|T],X,Y):- count(T,X,Z), Y is 1+Z.
+count([X1|T],X,Z):- X1\=X,count(T,X,Z).	
 % Opponents turn
-%min_to_move(Pos):-
-
+min_to_move(Pos):-
+	count(Pos,0,Y),
+	\+ 0 =:= mod 2.
 % Your turn
-%max_to_move(Pos):-
-
+max_to_move(Pos):-
+	count(Pos,0,Y),
+	0 =:= mod 2.
 
 move(Pos,X):-
 	select(0,Pos,1,X).
