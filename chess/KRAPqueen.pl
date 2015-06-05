ae8f93ab3890a7ca5cea9697ac77893140a4be9b
Michiel <michielmeer@live.nl>
2015-06-05 14:35:45 +0200

    added a not

--- chess/KRAPqueen.pl
+++ chess/KRAPqueen.pl
@@ -30,7 +30,7 @@ advice( mate_in_3,
 	( depth = 1 ) and legal then ( depth = 2 ) and legal ).
 	
 advice (avoidstalemate,
-	stalemate :
+	not stalemate :
 	not queenlost :
 	( depth = 0 ) and move_queen_away:
 	nomove ).
