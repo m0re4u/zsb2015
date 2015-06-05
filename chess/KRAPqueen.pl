f8e14e33ceda4dd2145b9db434f6398c1df6509b
Michiel <michielmeer@live.nl>
2015-06-05 14:40:04 +0200

    reorder on advice

--- chess/KRAPqueen.pl
+++ chess/KRAPqueen.pl
@@ -6,10 +6,10 @@
 % all rules
 
 else_rule :: if their_king_edge and kings_close
-	then [ mate_in_2, mate_in_3, squeeze, avoid_stalemate, approach, keeproom, divide_in_2, divide_in_3 ].
+	then [ mate_in_2, mate_in_3, squeeze, approach, keeproom, avoid_stalemate, divide_in_2, divide_in_3 ].
 
 else_rule :: if true
-	then [ squeeze, avoid_stalemate, approach, keeproom, divide_in_2, divide_in_3].
+	then [ squeeze, approach, keeproom,  avoid_stalemate, divide_in_2, divide_in_3].
 	
 % pieces of advice
 % structure:
